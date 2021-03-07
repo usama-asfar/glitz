4dc55e06cbc5621c86f9345eb7f6414f57809961
usama <usamaasfar.mua@gmail.com>
2021-03-07 23:48:52 +0530

    🚀 RELEASE: 0.1.0

--- mix.exs
+++ mix.exs
@@ -6,6 +6,9 @@ defmodule Glitz.MixProject do
       app: :glitz,
       version: "0.1.0",
       elixir: "~> 1.11",
+      name: "glitz",
+      description: "Elixir terminal styler",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +24,15 @@ defmodule Glitz.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/usama-asfar/glitz.git"}
     ]
   end
 end
